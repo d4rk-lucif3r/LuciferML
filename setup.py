26655f0e444951900f0c0f78460373368e110862
Arsh Anwar <45912425+d4rk-lucif3r@users.noreply.github.com>
2021-06-19 01:55:28 +0530

    Corrected Version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='lucifer-ml-dev',
     packages=['luciferml', 'luciferml.supervised',
               'luciferml.supervised.utils'],
-    version='0.0.2',
+    version='0.0.02',
     license='MIT',
     description="Automated ML by d4rk-lucif3r",
     long_description=long_description,
