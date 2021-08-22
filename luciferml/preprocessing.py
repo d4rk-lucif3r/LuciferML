f116e70c4afc5ddc209c2b846b52b7204a0aa9a6
Arsh Anwar <45912425+d4rk-lucif3r@users.noreply.github.com>
2021-08-22 12:48:53 +0530

    Update 0.0.55
    - Updated Docstring for preprocess' skewcorrect method

--- luciferml/preprocessing.py
+++ luciferml/preprocessing.py
@@ -39,38 +39,41 @@ class Preprocess:
 
     def skewcorrect(dataset, except_columns=[]) -> pd.DataFrame:
         """
-        This function returns two plots distplot and probability plot for non-normalized data and after normalizing the provided data.
+            Plots distplot and probability plot for non-normalized data and after normalizing the provided data.
+            Normalizes data using boxcox normalization
         
         Parameters:
             dataset : pd.DataFrame
                 Dataset on which skewness correction has to be done.
             except_columns : list
                 Columns for which skewness correction need not to be done.Default = []
-            
+                
+        :returns: Scaled Dataset
+        :rtype: pd.DataFrame        
+        
         Example:
 
          1) All Columns
 
-             from luciferml.preprocessing import Preprocess as prep
+                     from luciferml.preprocessing import Preprocess as prep
 
-             import pandas as pd
+                     import pandas as pd
 
-             dataset = pd.read_csv('/examples/Social_Network_Ads.csv')
+                     dataset = pd.read_csv('/examples/Social_Network_Ads.csv')
 
-             dataset = prep.skewcorrect(dataset)
+                     dataset = prep.skewcorrect(dataset)
 
          2) Except column/columns
 
-             from luciferml.preprocessing import Preprocess as prep
+                     from luciferml.preprocessing import Preprocess as prep
 
-             import pandas as pd
+                     import pandas as pd
 
-             dataset = pd.read_csv('/examples/Social_Network_Ads.csv')
+                     dataset = pd.read_csv('/examples/Social_Network_Ads.csv')
 
-             dataset = prep.skewcorrect(dataset,except_columns=['Purchased'])
+                     dataset = prep.skewcorrect(dataset,except_columns=['Purchased'])
         
-        :returns: Scaled Dataset
-        :rtype: pd.DataFrame
+
         """
         try:
             start = time.time()
