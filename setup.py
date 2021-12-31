66b0ea2a85284ce67bb72f204ad0865f1da0ac8e
Arsh Anwar <lucifer78908@gmail.com>
2022-01-01 00:07:19 +0530

    Update 0.0.71a
    - Fixed Shap Requirement missing
    
    Signed-off-by: Arsh Anwar <lucifer78908@gmail.com>

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "luciferml.supervised.utils.tuner.optuna",
         "luciferml.supervised.utils.tuner.optuna.objectives",
     ],
-    version="0.0.71",
+    version="0.0.71a",
     license="MIT",
     description="Automated ML by d4rk-lucif3r",
     long_description=long_description,
